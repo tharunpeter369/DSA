b5c097d542820604bc559409951cf7b2e95713c7
tharun <tharunpeter369@gmail.com>
2022-11-03 00:18:18 +0530

    sample done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # DSA
+# check currying
+# memoize
+# call,apply,and bind
--- sample.js
+++ sample.js
@@ -1,18 +1,37 @@
-const nemo = ['nemo']
-// console.log("🚀 ~ file: sample.js ~ line 2 ~ nemo", nemo)
+// const nemo = ['nemo']
+// // console.log("🚀 ~ file: sample.js ~ line 2 ~ nemo", nemo)
 
-const everyone = ['hello', "hiii", "shibin", "tharun", 'nemo', "fighter fish"]
+// const everyone = ['hello', "hiii", "shibin", "tharun", 'nemo', "fighter fish"]
 
-const large = new Array(1000000).fill("tharun")
-const t1 = performance.now()
-function findNemo(nemo) {
-    for (i = 0; i < nemo.length; i++) {
-        if (nemo[i] == 'nemo') {
-            console.log("🚀 ~ file: sample.js ~ line 6 ~ findNemo ~ nemo", nemo[i])
+// const large = new Array(1000000).fill("tharun")
+// const t1 = performance.now()
+// function findNemo(nemo) {
+//     for (i = 0; i < nemo.length; i++) {
+//         if (nemo[i] == 'nemo') {
+//             console.log("🚀 ~ file: sample.js ~ line 6 ~ findNemo ~ nemo", nemo[i])
+//         }
+//     }
+//     const t2 = performance.now()
+//     console.log('difference', t2 - t1);
+// }
+
+// findNemo(large)
+
+function x() {
+    var a = 4;
+    function y() {
+        var i = 2
+        function q() {
+            console.log(a, i);
         }
+        q()
     }
-    const t2 = performance.now()
-    console.log('difference', t2 - t1);
+    a = 20
+    // return y
+    y()
 }
 
-findNemo(large)
+x()
+
+
+// console.log(x()());
