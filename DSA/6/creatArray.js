f8b170f039dd292b322dbbc436ff6f475371dee4
tharun <tharunpeter369@gmail.com>
2022-11-14 22:52:19 +0530

    create array

--- DSA/6/creatArray.js
+++ DSA/6/creatArray.js
@@ -21,19 +21,28 @@ class MyArray {
 
     deleteAtIndex(index) {
         const item = this.data[index];
+       return this.shiftItems(index)
     }
-    
-    shiftItems(index) {
 
+    shiftItems(index) {
+        const item = this.data[index]
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
+        }
+        delete this.data[this.length-1];
+        this.length--;
+        return this.data
     }
 }
 
 const array1 = new MyArray
+console.log(array1);
 
 console.log(array1.push(1));
 console.log(array1.push(2));
+console.log(array1.push(7));
+console.log(array1.push(8));
 // console.log(array1.get(0));
-console.log(array1.pop());
+// console.log(array1.pop());
+console.log(array1.deleteAtIndex(2));
 
-
-// console.log(array1);
