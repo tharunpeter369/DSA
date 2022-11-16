91e79d639267675edaca40bc717c4df96ce57403
tharun <tharunpeter369@gmail.com>
2022-11-16 23:53:58 +0530

    modified

---
+++ DSA/7Hashtable/HasgTableGeneral.js
@@ -0,0 +1,11 @@
+// we will see them databases and caches
+
+// What does idempotent mean in programming?
+// Idempotence, in programming and mathematics, is a property of some operations such that no matter how many times you execute them,
+//  you achieve the same result. In programming, idempotence can be a property of many different code elements, including functions, methods,
+//  requests and statements.
+
+// in collition in hashtable is o(n)
+
+// Map() :- in map we can use any data type as key but in objnect only string as key and no duplicates are allowed
+// Set() :- in set we cant set duplicates like array and it only strores key
