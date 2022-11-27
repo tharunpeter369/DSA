bc41b6cd84c2707a385f24e2911b643b3c71284b
tharun <tharunpeter369@gmail.com>
2022-11-27 22:58:30 +0530

    need to be clear

--- DSA/8Linkedlist/excersise1_96.js
+++ DSA/8Linkedlist/excersise1_96.js
@@ -17,3 +17,28 @@
 
 //now lets create linked list
 
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    }
+    this.tail.next = newNode,
+      this.tail = newNode,
+      this.length = this.length + 1;
+      return this
+  }
+}
+const myLinkedList = new LinkedList(10);
+console.log(myLinkedList, "🥰");
+let ap1 = myLinkedList.append(5)
+myLinkedList.append(16)
+console.log(ap1,"ap1🙂");
