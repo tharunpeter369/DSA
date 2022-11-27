def18e9225f583d42bc79b5f62c09cd9003ca67f
tharun <tharunpeter369@gmail.com>
2022-11-27 22:25:07 +0530

    modified

---
+++ DSA/8Linkedlist/excersise1_96.js
@@ -0,0 +1,19 @@
+// example of linked list
+
+// 10 -- > 5 -- > 16
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       }
+//     }
+//   }
+// }
+
+//now lets create linked list
+
