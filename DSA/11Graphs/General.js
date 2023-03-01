f52604019243fd072e798767105392acb21bf5c8
tharun <tharunpeter369@gmail.com>
2023-03-01 09:24:22 +0530

    graph added

---
+++ DSA/11Graphs/General.js
@@ -0,0 +1,19 @@
+// a graph is pair of set values related in a pair vise fashion
+
+
+// we can see the graph in face booke of connectin people and in google maps for connectin places(shortest path)
+
+// linkes list are type of trees and trees are type of graph 
+
+
+// type of graph   // image 3
+// 1. directed graph   eg: one way road twitter am connected to celebrati and that celibraty not connected to me
+// 2. undirected graph  eg: two ways road it like fb is like am connected to fried and that friend also connected to me
+
+// another type is Wighted graph(nodes/vertex have values) //image 4
+// 1.Wighted graph (both nodes/vertex and edges have values) 
+// 2.unweighted graph (only nodes/vertex have values) eg: google maps for finding shortest path
+
+// another type image 5
+// 1.cyclic graph
+// 2.acyclic graph
